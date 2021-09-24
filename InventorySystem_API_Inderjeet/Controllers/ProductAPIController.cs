28032b5a5e513ad2028effa2e70e2a1b1acd56c4
Indersidhu362@gmail.com <Indersidhu362@gmail.com>
2021-09-24 07:50:09 +0300

    Add project files.

---
+++ InventorySystem_API_Inderjeet/Controllers/ProductAPIController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventorySystem_API_Inderjeet.Controllers
+{
+    public class ProductAPIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
